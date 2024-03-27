08edfd6a3fa53eb8b788ad7b1b4dcda83f57db3a
woodi97 <woodi.daily@gmail.com>
2024-03-27 18:01:33 +0900

    feat: random reviewer(checkpoint)

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,46 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
+import * as slack from '@slack/web-api'
 import { wait } from './wait'
 
-/**
- * The main function for the action.
- * @returns {Promise<void>} Resolves when the action is complete.
- */
+const githubClient = github.getOctokit(core.getInput('token'))
+const slackClient = new slack.WebClient(core.getInput('slackToken'))
+
+const slackConversationId = core.getInput('slackConversationId')
+
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
+    const selectedReviewer = selectRandomReviewer()
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    await githubClient.rest.pulls.requestReviewers({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      pull_number: github.context.issue.number,
+      reviewers: [selectedReviewer.githubName]
+    })
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
   }
+}
+
+function parseCandidates(candidates: string): { githubName: string }[] {
+  return candidates.split(',').map(person => {
+    const [githubName] = person.split(':')
+    return { githubName }
+  })
+}
+
+function getCandidates() {
+  const candidates = core.getInput('candidates')
+  return parseCandidates(candidates)
+}
+
+function selectRandomReviewer() {
+  const prCreator = github.context.payload.pull_request?.user.login
+  const candidateReviewer = getCandidates().filter(
+    person => person.githubName !== prCreator
+  )
+
+  return candidateReviewer[Math.floor(Math.random() * candidateReviewer.length)]
 }
---
+++ src/utils/slack/index.ts
@@ -0,0 +1,32 @@
+import { ChatPostMessageArguments } from '@slack/web-api'
+import { App } from '@slack/bolt'
+import {
+  SLACK_BOT_SIGNING_SECRET,
+  SLACK_BOT_TOKEN,
+  TARGET_SLACK_CHANNEL_ID
+} from '../input'
+// import { messageTitle } from './messages/anyCountMessage';
+
+const slackClient = new App({
+  token: SLACK_BOT_TOKEN,
+  signingSecret: SLACK_BOT_SIGNING_SECRET
+})
+
+export function sendMessage(args: ChatPostMessageArguments) {
+  return slackClient.client.chat.postMessage(args)
+}
+
+function getChannelMessages() {
+  return slackClient.client.conversations.history({
+    channel: TARGET_SLACK_CHANNEL_ID
+  })
+}
+
+export async function getSentMessages() {
+  const { messages } = await getChannelMessages()
+  return messages
+    ?.filter(({ bot_id }) => bot_id != null)
+    .filter(({ type }) => type === 'message')
+    .filter(({ text }) => text === `This content can't be displayed.`)
+    .filter(({ blocks }) => blocks != null)
+}
